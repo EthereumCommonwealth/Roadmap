ecfa1d65867698c683e6cdca54bf13874cb4bfea
Dexaran <dexaran820@gmail.com>
2017-07-12 12:00:08 +0400

    Create TemporaryVotepool.sol

---
+++ Voting/TemporaryVotepool.sol
@@ -0,0 +1,59 @@
+pragma solidity ^0.4.11;
+
+contract ERC223Basic {
+    uint public totalSupply;
+    function balanceOf(address who) constant returns (uint);
+    function transfer(address to, uint value);
+    function transfer(address to, uint value, bytes data);
+    event Transfer(address indexed from, address indexed to, uint value, bytes indexed data);
+}
+
+
+contract Temporary_Votepool
+{
+    address public owner = msg.sender;
+    address public token = 0x345a9e6c44d546dae5141700372986c4bb532e3d;
+    
+    string public name = "Don't care";
+    
+    mapping (address => uint256) public deposited;
+    
+    function Vote_Name() constant returns (string _name) { return name; }
+    
+    function tokenFallback(address _from, uint256 _value, bytes _data)
+    {
+        if(msg.sender == token)
+        {
+            deposited[_from] += _value;
+        }
+    }
+    
+    function Refund(address _who) only_owner
+    {
+        ERC223Basic Token = ERC223Basic(token);
+        Token.transfer(_who, deposited[_who]);
+        deposited[_who] = 0; 
+    }
+    
+    function Withdraw()
+    {
+        ERC223Basic Token = ERC223Basic(token);
+        Token.transfer(msg.sender, deposited[msg.sender]);
+        deposited[msg.sender] = 0; 
+    }
+    
+    function Total_Score() constant returns (uint256)
+    {
+        ERC223Basic Token = ERC223Basic(token);
+        return Token.balanceOf(this);
+    }
+    
+    modifier only_owner
+    {
+        if(msg.sender != owner)
+        {
+            throw;
+        }
+        _;
+    }
+}
